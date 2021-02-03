4c6b228bb731f0f27de71d2615fa9689ce8e4c86
Alex Studer <alex@studer.dev>
2021-02-02 23:40:27 -0500

    formula/roamer: move bottle section to be before depends_on
    thanks to brew test-bot for noticing this after only eight whole minutes of
    thinking

--- Formula/roamer.rb
+++ Formula/roamer.rb
@@ -4,13 +4,13 @@ class Roamer < Formula
   url "https://github.com/thatoddmailbox/roamer/archive/v0.1.0.tar.gz"
   sha256 "5753582c665d156b596bbff4184496e68d452e356dda0954cafc6f2d5b9cf343"
 
-  depends_on "go" => :build
-
   bottle do
     root_url "https://raw.githubusercontent.com/thatoddmailbox/homebrew-tap/master/bin"
     cellar :any_skip_relocation
     sha256 "2074b5ae3092331286505f22485f9b8b891ab10c0dbe8afad0e279922245ff0f" => :catalina
   end
+
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args, "./cli/"
