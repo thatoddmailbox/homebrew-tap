412f43ec4fbc46096a7b529b56f748495269cedd
Alex Studer <alex@studer.dev>
2022-09-30 01:30:27 -0400

    formula/roamer: align bottle digests, to make the style checker happy

--- Formula/roamer.rb
+++ Formula/roamer.rb
@@ -6,12 +6,12 @@ class Roamer < Formula
 
   bottle do
     root_url "https://raw.githubusercontent.com/thatoddmailbox/homebrew-tap/master/bin"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
-    sha256 cellar: :any_skip_relocation, big_sur: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
-    sha256 cellar: :any_skip_relocation, monterey: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
-    sha256 cellar: :any_skip_relocation, ventura: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:   "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey:  "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:   "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, big_sur:         "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, monterey:        "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, ventura:         "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
   end
 
   depends_on "go" => :build
