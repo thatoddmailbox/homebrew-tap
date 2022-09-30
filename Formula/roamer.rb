b091a966938d6e2f9621c14119060e68e04b5417
Alex Studer <alex@studer.dev>
2022-09-30 00:03:11 -0400

    update roamer formula to v0.1.1, and add corresponding binary bottles

--- Formula/roamer.rb
+++ Formula/roamer.rb
@@ -1,12 +1,17 @@
 class Roamer < Formula
   desc "Tool that makes handling database migrations easy"
   homepage "https://github.com/thatoddmailbox/roamer"
-  url "https://github.com/thatoddmailbox/roamer/archive/v0.1.0.tar.gz"
-  sha256 "5753582c665d156b596bbff4184496e68d452e356dda0954cafc6f2d5b9cf343"
+  url "https://github.com/thatoddmailbox/roamer/archive/v0.1.1.tar.gz"
+  sha256 "88ceeec110b3918ebc947b9aa6a4a33b96bcd77baeea952117be5067296d4126"
 
   bottle do
     root_url "https://raw.githubusercontent.com/thatoddmailbox/homebrew-tap/master/bin"
-    sha256 cellar: :any_skip_relocation, catalina: "2074b5ae3092331286505f22485f9b8b891ab10c0dbe8afad0e279922245ff0f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, big_sur: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, monterey: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
+    sha256 cellar: :any_skip_relocation, ventura: "e99608e27d8e0d2ff16889ff24f41f6179022dd8e82c723526b1323622061e8f"
   end
 
   depends_on "go" => :build
